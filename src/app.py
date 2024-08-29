ed15f91c7a7a3061a1d4482a1d4481fb44030464
naumanrafique12 <naumanrafique10.com>
2024-08-29 21:55:46 +0500

    PC3

--- src/app.py
+++ src/app.py
@@ -16,6 +16,9 @@ iq_scores = np.random.randint(85, 130, 50)
 # Generate CGPA scores (random numbers between 2.0 and 4.0)
 cgpa_scores = np.round(np.random.uniform(2.0, 4.0, 50), 2)
 
+# Generate CGPA scores (random numbers between 2.0 and 4.0)
+cgpa_scores_2 = np.round(np.random.uniform(2.0, 4.0, 50), 2)
+
 # Generate Placed status (0 or 1, where 1 means placed)
 placed_status = np.random.choice([0, 1], 50)
 
@@ -23,13 +26,14 @@ placed_status = np.random.choice([0, 1], 50)
 student_data = pd.DataFrame({
     'IQ': iq_scores,
     'CGPA': cgpa_scores,
+    'CGPA2': cgpa_scores,
+    'CGPA3': cgpa_scores_2,
+    'CGPA4': cgpa_scores_2,
+    'CGPA5': cgpa_scores_2,
     'Placed': placed_status
 })
 
 df = student_data.copy()
-
- 
-
 # Separating features and target variable
 X = df.drop(columns=['Placed'])
 y = df['Placed']
